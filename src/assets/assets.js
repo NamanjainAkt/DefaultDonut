a08d7a8cd06819cea7c8c9186201a69e4c1f6e0b
Naman Jain <162738854+NamanjainAkt@users.noreply.github.com>
2025-05-14 23:43:48 +0530

    bugs fixed

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,10 +1,10 @@
 import Abhinav_Gupta from './Abhinav_Gupta.png';
 import Deepak_Vishwakarma from './Deepak_Vishwakarma.png';
 import Khushi_Kosriya from './Khushi_Kosriya.png';
-import  Ketan_Kumar_Sahu from './Ketan_Kumar_Sahu.png';
+import  Ketan_Kumar_Sahu from './Ketan_kumar_Sahu.png';
 import Piyush_Sharma from './Piyush_Sharma.png';
-import Uday_Pratap_Sahu from './Uday_pratap_sahu.png';
-import Vaibhav_Thakur from './Vaibhav_thakur.png';
+import Uday_Pratap_Sahu from './Uday_Pratap_Sahu.png';
+import Vaibhav_Thakur from './Vaibhav_Thakur.png';
 import Warisha_Fatima from './Warisha_Fatima.png';
 
 export const assets = {
